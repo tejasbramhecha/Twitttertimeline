7d2f5669329c30ef4d3fc6aab572eb204653be88
tejasbramhecha <jaintejas511@gmail.com>
2016-06-29 10:36:18 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+  <head>
+    
+	
+  </head>
+  <body>
+	<div style="background-color:cccccc; width:1000px; height:940px; margin-left:245px;">
+			<div style="border-size:1px; border-color:black; display:block; background-color:ff9900; margin-top:5px; padding-top:10px; padding-bottom:5px; 
+			 margin-left:80px; margin-right:80px;" width="1500px" height="500px">
+			 <h1> Hello world </h1>
+	</div>
+	</div>
+    
+  </body>
+</html>
